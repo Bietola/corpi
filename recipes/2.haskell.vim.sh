4da9beccb730a5431eef495899371a7a8e061a2b
Bietola <dincio.montesi@gmail.com>
2021-05-23 03:22:38 +0200

    Finish impl. of `cook`

--- recipes/2.haskell.vim.sh
+++ recipes/2.haskell.vim.sh
@@ -1,18 +1,1 @@
-#!/bin/sh
-
-usage() {
-  echo "Usage: 1.haskell.vim"
-
-  exit 1
-}
-
-[ $# -ne 0 ] && usage
-
-ROOT=/home/dincio/corpi
-cd "$ROOT/bin"
-
-# Recipe
-./corpi -n 1
-./recs -n 22 | ./1.proc
-
-cd - >/dev/null
+./recs -n 12 -n 16 -n 17 -n 19 -n 21 -n 22 | ./1.proc
