ee29a3d50872dcebe1c73d794d1a7ee45ff604c2
Bietola <dincio.montesi@gmail.com>
2021-05-26 12:46:17 +0200

    Add git commit control to `0.scrape-haskell-from-gh`

--- recipes/6.haskell.vim.sh
+++ recipes/6.haskell.vim.sh
@@ -9,11 +9,7 @@ RECS='12 16 17 19 21 22 23 24 25 26 27'
             ) \
         -c <({
 
-                ./0.scrape-haskell-from-gh -C 'https://github.com/glguy/advent2017' |\
-                    ./1.proc-src
-
-                ./0.scrape-haskell-from-gh -C 'https://github.com/Bietola/streamkey' \
-                    492addee6725398797081ba8481454fa779dcf63 |\
+                ./0.scrape-haskell-from-gh -nd 'https://github.com/glguy/advent2017' |\
                     ./1.proc-src
 
                 # Arrows where used extensively in ghci non-vim mode in this age... which should not be the case,
